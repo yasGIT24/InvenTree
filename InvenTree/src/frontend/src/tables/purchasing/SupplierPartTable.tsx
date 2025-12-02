af8e6c14236181bbd21c905866aea04dcfcf8820
yasGIT24 <101236038+yasGIT24@users.noreply.github.com>
2025-12-02 19:53:09 +0530

    Uploading SupplierPartTable.tsx

---
+++ InvenTree/src/frontend/src/tables/purchasing/SupplierPartTable.tsx
@@ -0,0 +1,267 @@
+import { t } from '@lingui/core/macro';
+import { Text } from '@mantine/core';
+import { type ReactNode, useCallback, useMemo, useState } from 'react';
+
+import { AddItemButton } from '@lib/components/AddItemButton';
+import {
+  type RowAction,
+  RowDeleteAction,
+  RowEditAction
+} from '@lib/components/RowActions';
+import { ApiEndpoints } from '@lib/enums/ApiEndpoints';
+import { ModelType } from '@lib/enums/ModelType';
+import { UserRoles } from '@lib/enums/Roles';
+import { apiUrl } from '@lib/functions/Api';
+import type { TableFilter } from '@lib/types/Filters';
+import type { TableColumn } from '@lib/types/Tables';
+import { useSupplierPartFields } from '../../forms/CompanyForms';
+import {
+  useCreateApiFormModal,
+  useDeleteApiFormModal,
+  useEditApiFormModal
+} from '../../hooks/UseForm';
+import { useTable } from '../../hooks/UseTable';
+import { useUserState } from '../../states/UserState';
+import {
+  BooleanColumn,
+  CompanyColumn,
+  DecimalColumn,
+  DescriptionColumn,
+  LinkColumn,
+  NoteColumn,
+  PartColumn
+} from '../ColumnRenderers';
+import { InvenTreeTable } from '../InvenTreeTable';
+import { TableHoverCard } from '../TableHoverCard';
+
+/*
+ * Construct a table listing supplier parts
+ */
+
+export function SupplierPartTable({
+  params
+}: Readonly<{ params: any }>): ReactNode {
+  const table = useTable('supplierparts');
+
+  const user = useUserState();
+
+  // Construct table columns for this table
+  const tableColumns: TableColumn[] = useMemo(() => {
+    return [
+      PartColumn({
+        switchable: 'part' in params,
+        part: 'part_detail'
+      }),
+      {
+        accessor: 'supplier',
+        sortable: true,
+        render: (record: any) => (
+          <CompanyColumn company={record?.supplier_detail} />
+        )
+      },
+      {
+        accessor: 'SKU',
+        title: t`Supplier Part`,
+        sortable: true
+      },
+      DescriptionColumn({}),
+      {
+        accessor: 'manufacturer',
+        title: t`Manufacturer`,
+        sortable: true,
+        render: (record: any) => (
+          <CompanyColumn company={record?.manufacturer_detail} />
+        )
+      },
+      {
+        accessor: 'MPN',
+
+        sortable: true,
+        title: t`MPN`,
+        render: (record: any) => record?.manufacturer_part_detail?.MPN
+      },
+      BooleanColumn({
+        accessor: 'active',
+        title: t`Active`,
+        sortable: true,
+        switchable: true,
+        defaultVisible: false
+      }),
+      DecimalColumn({
+        accessor: 'in_stock',
+        sortable: true
+      }),
+      {
+        accessor: 'packaging',
+        sortable: true,
+        defaultVisible: false
+      },
+      {
+        accessor: 'pack_quantity',
+        sortable: true,
+
+        render: (record: any) => {
+          const part = record?.part_detail ?? {};
+
+          const extra = [];
+
+          if (part.units) {
+            extra.push(
+              <Text key='base'>
+                {t`Base units`} : {part.units}
+              </Text>
+            );
+          }
+
+          return (
+            <TableHoverCard
+              value={record.pack_quantity}
+              extra={extra}
+              title={t`Pack Quantity`}
+            />
+          );
+        }
+      },
+      LinkColumn({}),
+      NoteColumn({}),
+      {
+        accessor: 'available',
+        sortable: true,
+        defaultVisible: false,
+        render: (record: any) => {
+          const extra = [];
+
+          if (record.availablility_updated) {
+            extra.push(
+              <Text>
+                {t`Updated`} : {record.availablility_updated}
+              </Text>
+            );
+          }
+
+          return <TableHoverCard value={record.available} extra={extra} />;
+        }
+      }
+    ];
+  }, [params]);
+
+  const supplierPartFields = useSupplierPartFields({
+    partId: params?.part
+  });
+
+  const addSupplierPart = useCreateApiFormModal({
+    url: ApiEndpoints.supplier_part_list,
+    title: t`Add Supplier Part`,
+    fields: supplierPartFields,
+    initialData: {
+      part: params?.part,
+      supplier: params?.supplier
+    },
+    table: table,
+    successMessage: t`Supplier part created`
+  });
+
+  const tableActions = useMemo(() => {
+    return [
+      <AddItemButton
+        key='add-supplier-part'
+        tooltip={t`Add supplier part`}
+        onClick={() => addSupplierPart.open()}
+        hidden={!user.hasAddRole(UserRoles.purchase_order)}
+      />
+    ];
+  }, [user]);
+
+  const tableFilters: TableFilter[] = useMemo(() => {
+    return [
+      {
+        name: 'active',
+        label: t`Active`,
+        description: t`Show active supplier parts`
+      },
+      {
+        name: 'part_active',
+        label: t`Active Part`,
+        description: t`Show active internal parts`
+      },
+      {
+        name: 'supplier_active',
+        label: t`Active Supplier`,
+        description: t`Show active suppliers`
+      },
+      {
+        name: 'has_stock',
+        label: t`In Stock`,
+        description: t`Show supplier parts with stock`
+      }
+    ];
+  }, []);
+
+  const editSupplierPartFields = useSupplierPartFields({});
+
+  const [selectedSupplierPart, setSelectedSupplierPart] = useState<number>(0);
+
+  const editSupplierPart = useEditApiFormModal({
+    url: ApiEndpoints.supplier_part_list,
+    pk: selectedSupplierPart,
+    title: t`Edit Supplier Part`,
+    fields: editSupplierPartFields,
+    table: table
+  });
+
+  const deleteSupplierPart = useDeleteApiFormModal({
+    url: ApiEndpoints.supplier_part_list,
+    pk: selectedSupplierPart,
+    title: t`Delete Supplier Part`,
+    table: table
+  });
+
+  // Row action callback
+  const rowActions = useCallback(
+    (record: any): RowAction[] => {
+      return [
+        RowEditAction({
+          hidden: !user.hasChangeRole(UserRoles.purchase_order),
+          onClick: () => {
+            setSelectedSupplierPart(record.pk);
+            editSupplierPart.open();
+          }
+        }),
+        RowDeleteAction({
+          hidden: !user.hasDeleteRole(UserRoles.purchase_order),
+          onClick: () => {
+            setSelectedSupplierPart(record.pk);
+            deleteSupplierPart.open();
+          }
+        })
+      ];
+    },
+    [user, editSupplierPartFields]
+  );
+
+  return (
+    <>
+      {addSupplierPart.modal}
+      {editSupplierPart.modal}
+      {deleteSupplierPart.modal}
+      <InvenTreeTable
+        url={apiUrl(ApiEndpoints.supplier_part_list)}
+        tableState={table}
+        columns={tableColumns}
+        props={{
+          params: {
+            ...params,
+            part_detail: true,
+            supplier_detail: true,
+            manufacturer_detail: true
+          },
+          rowActions: rowActions,
+          enableDownload: true,
+          tableActions: tableActions,
+          tableFilters: tableFilters,
+          modelType: ModelType.supplierpart
+        }}
+      />
+    </>
+  );
+}
