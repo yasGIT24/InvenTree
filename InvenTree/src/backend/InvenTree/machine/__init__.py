d706eca794972ee4b699e3a0408cc1d5a1c31620
yasGIT24 <101236038+yasGIT24@users.noreply.github.com>
2025-12-02 19:26:37 +0530

    Uploading __init__.py

---
+++ InvenTree/src/backend/InvenTree/machine/__init__.py
@@ -0,0 +1,4 @@
+from machine.machine_type import BaseDriver, BaseMachineType, MachineStatus
+from machine.registry import registry
+
+__all__ = ['BaseDriver', 'BaseMachineType', 'MachineStatus', 'registry']
