0a2661b19a725dae1e70da1e7f5d76ae84568a3c
yasGIT24 <101236038+yasGIT24@users.noreply.github.com>
2025-12-02 19:50:54 +0530

    Uploading Company.tsx

---
+++ InvenTree/src/frontend/src/components/render/Company.tsx
@@ -0,0 +1,109 @@
+import type { ReactNode } from 'react';
+
+import { ModelType } from '@lib/enums/ModelType';
+import { getDetailUrl } from '@lib/functions/Navigation';
+import { type InstanceRenderInterface, RenderInlineModel } from './Instance';
+
+/**
+ * Inline rendering of a single Address instance
+ */
+export function RenderAddress({
+  instance
+}: Readonly<InstanceRenderInterface>): ReactNode {
+  const text = [
+    instance.country,
+    instance.postal_code,
+    instance.postal_city,
+    instance.province,
+    instance.line1,
+    instance.line2
+  ]
+    .filter(Boolean)
+    .join(', ');
+
+  return <RenderInlineModel primary={instance.title} secondary={text} />;
+}
+
+/**
+ * Inline rendering of a single Company instance
+ */
+export function RenderCompany(
+  props: Readonly<InstanceRenderInterface>
+): ReactNode {
+  const { instance } = props;
+
+  return (
+    <RenderInlineModel
+      {...props}
+      image={instance.thumbnail || instance.image}
+      primary={instance.name}
+      suffix={instance.description}
+      url={
+        props.link ? getDetailUrl(ModelType.company, instance.pk) : undefined
+      }
+    />
+  );
+}
+
+/**
+ * Inline rendering of a single Contact instance
+ */
+export function RenderContact({
+  instance
+}: Readonly<InstanceRenderInterface>): ReactNode {
+  return <RenderInlineModel primary={instance.name} />;
+}
+
+/**
+ * Inline rendering of a single SupplierPart instance
+ */
+export function RenderSupplierPart(
+  props: Readonly<InstanceRenderInterface>
+): ReactNode {
+  const { instance } = props;
+  const supplier = instance.supplier_detail ?? {};
+  const part = instance.part_detail ?? {};
+
+  return (
+    <RenderInlineModel
+      {...props}
+      primary={supplier?.name}
+      secondary={instance.SKU}
+      image={
+        part?.thumbnail ?? part?.image ?? supplier?.thumbnail ?? supplier?.image
+      }
+      suffix={part.full_name}
+      url={
+        props.link
+          ? getDetailUrl(ModelType.supplierpart, instance.pk)
+          : undefined
+      }
+    />
+  );
+}
+
+/**
+ * Inline rendering of a single ManufacturerPart instance
+ */
+export function RenderManufacturerPart(
+  props: Readonly<InstanceRenderInterface>
+): ReactNode {
+  const { instance } = props;
+  const part = instance.part_detail ?? {};
+  const manufacturer = instance.manufacturer_detail ?? {};
+
+  return (
+    <RenderInlineModel
+      {...props}
+      primary={manufacturer.name}
+      secondary={instance.MPN}
+      suffix={part.full_name}
+      image={manufacturer?.thumbnail ?? manufacturer.image}
+      url={
+        props.link
+          ? getDetailUrl(ModelType.manufacturerpart, instance.pk)
+          : undefined
+      }
+    />
+  );
+}
