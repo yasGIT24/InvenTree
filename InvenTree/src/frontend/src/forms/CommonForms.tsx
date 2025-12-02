946c29abf8732882c7014541c0d4b88ed36d214c
yasGIT24 <101236038+yasGIT24@users.noreply.github.com>
2025-12-02 19:30:19 +0530

    Uploading CommonForms.tsx

---
+++ InvenTree/src/frontend/src/forms/CommonForms.tsx
@@ -0,0 +1,89 @@
+import { IconUsers } from '@tabler/icons-react';
+import { useMemo, useState } from 'react';
+
+import type { ApiFormFieldSet } from '@lib/types/Forms';
+import type {
+  StatusCodeInterface,
+  StatusCodeListInterface
+} from '../components/render/StatusRenderer';
+import { useGlobalStatusState } from '../states/GlobalStatusState';
+
+export function projectCodeFields(): ApiFormFieldSet {
+  return {
+    code: {},
+    description: {},
+    responsible: {
+      icon: <IconUsers />
+    }
+  };
+}
+
+export function useCustomStateFields(): ApiFormFieldSet {
+  // Status codes
+  const statusCodes = useGlobalStatusState();
+
+  // Selected base status class
+  const [statusClass, setStatusClass] = useState<string>('');
+
+  // Construct a list of status options based on the selected status class
+  const statusOptions: any[] = useMemo(() => {
+    const options: any[] = [];
+
+    const valuesList = Object.values(statusCodes.status ?? {}).find(
+      (value: StatusCodeListInterface) => value.status_class === statusClass
+    );
+
+    Object.values(valuesList?.values ?? {}).forEach(
+      (value: StatusCodeInterface) => {
+        options.push({
+          value: value.key,
+          display_name: value.label
+        });
+      }
+    );
+
+    return options;
+  }, [statusCodes, statusClass]);
+
+  return useMemo(() => {
+    return {
+      reference_status: {
+        onValueChange(value) {
+          setStatusClass(value);
+        }
+      },
+      logical_key: {
+        field_type: 'choice',
+        choices: statusOptions
+      },
+      key: {},
+      name: {},
+      label: {},
+      color: {},
+      model: {}
+    };
+  }, [statusOptions]);
+}
+
+export function customUnitsFields(): ApiFormFieldSet {
+  return {
+    name: {},
+    definition: {},
+    symbol: {}
+  };
+}
+
+export function extraLineItemFields(): ApiFormFieldSet {
+  return {
+    order: {
+      hidden: true
+    },
+    reference: {},
+    description: {},
+    quantity: {},
+    price: {},
+    price_currency: {},
+    notes: {},
+    link: {}
+  };
+}
