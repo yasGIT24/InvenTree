db37caee1c2c7e776e3266d0d255b8df3b5cd1a1
yasGIT24 <101236038+yasGIT24@users.noreply.github.com>
2025-12-02 19:26:28 +0530

    Uploading apps.py

---
+++ InvenTree/src/backend/InvenTree/machine/apps.py
@@ -0,0 +1,43 @@
+"""Django machine app config."""
+
+from django.apps import AppConfig
+from django.db.utils import OperationalError, ProgrammingError
+
+import structlog
+
+from InvenTree.ready import (
+    canAppAccessDatabase,
+    isImportingData,
+    isInMainThread,
+    isPluginRegistryLoaded,
+    isRunningMigrations,
+)
+
+logger = structlog.get_logger('inventree')
+
+
+class MachineConfig(AppConfig):
+    """AppConfig class for the machine app."""
+
+    name = 'machine'
+
+    def ready(self) -> None:
+        """Initialization method for the machine app."""
+        if (
+            not canAppAccessDatabase(allow_test=True)
+            or not isPluginRegistryLoaded()
+            or isRunningMigrations()
+            or isImportingData()
+        ):
+            logger.debug('Machine app: Skipping machine loading sequence')
+            return
+
+        from machine import registry
+
+        try:
+            logger.info('Loading InvenTree machines')
+            if not registry.is_ready:
+                registry.initialize(main=isInMainThread())
+        except (OperationalError, ProgrammingError):
+            # Database might not yet be ready
+            logger.warn('Database was not ready for initializing machines')
