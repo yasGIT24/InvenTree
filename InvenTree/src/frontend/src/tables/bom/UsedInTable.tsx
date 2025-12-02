b8cdb32ceda889f2b13ba1912a27dd0d6c2ce124
yasGIT24 <101236038+yasGIT24@users.noreply.github.com>
2025-12-02 19:52:17 +0530

    Uploading UsedInTable.tsx

---
+++ InvenTree/src/frontend/src/tables/bom/UsedInTable.tsx
@@ -0,0 +1,119 @@
+import { t } from '@lingui/core/macro';
+import { Group, Text } from '@mantine/core';
+import { useMemo } from 'react';
+
+import { ApiEndpoints } from '@lib/enums/ApiEndpoints';
+import { ModelType } from '@lib/enums/ModelType';
+import { apiUrl } from '@lib/functions/Api';
+import type { TableFilter } from '@lib/types/Filters';
+import type { TableColumn } from '@lib/types/Tables';
+import { formatDecimal } from '../../defaults/formatters';
+import { useTable } from '../../hooks/UseTable';
+import {
+  DescriptionColumn,
+  PartColumn,
+  ReferenceColumn
+} from '../ColumnRenderers';
+import { InvenTreeTable } from '../InvenTreeTable';
+
+/*
+ * For a given part, render a table showing all the assemblies the part is used in
+ */
+export function UsedInTable({
+  partId,
+  params = {}
+}: Readonly<{
+  partId: number;
+  params?: any;
+}>) {
+  const table = useTable('usedin');
+
+  const tableColumns: TableColumn[] = useMemo(() => {
+    return [
+      PartColumn({
+        title: t`Assembly`,
+        part: 'part_detail'
+      }),
+      {
+        accessor: 'part_detail.IPN',
+        sortable: false,
+        title: t`IPN`
+      },
+      {
+        accessor: 'part_detail.revision',
+        title: t`Revision`,
+        sortable: true,
+        defaultVisible: false
+      },
+      DescriptionColumn({
+        accessor: 'part_detail.description'
+      }),
+      PartColumn({
+        accessor: 'sub_part',
+        sortable: true,
+        title: t`Component`,
+        part: 'sub_part_detail'
+      }),
+      {
+        accessor: 'quantity',
+        switchable: false,
+        render: (record: any) => {
+          const quantity = formatDecimal(record.quantity);
+          const units = record.sub_part_detail?.units;
+
+          return (
+            <Group justify='space-between' grow wrap='nowrap'>
+              <Text>{quantity}</Text>
+              {units && <Text size='xs'>{units}</Text>}
+            </Group>
+          );
+        }
+      },
+      ReferenceColumn({})
+    ];
+  }, [partId]);
+
+  const tableFilters: TableFilter[] = useMemo(() => {
+    return [
+      {
+        name: 'inherited',
+        label: t`Inherited`,
+        description: t`Show inherited items`
+      },
+      {
+        name: 'optional',
+        label: t`Optional`,
+        description: t`Show optional items`
+      },
+      {
+        name: 'part_active',
+        label: t`Active`,
+        description: t`Show active assemblies`
+      },
+      {
+        name: 'part_trackable',
+        label: t`Trackable`,
+        description: t`Show trackable assemblies`
+      }
+    ];
+  }, [partId]);
+
+  return (
+    <InvenTreeTable
+      url={apiUrl(ApiEndpoints.bom_list)}
+      tableState={table}
+      columns={tableColumns}
+      props={{
+        params: {
+          ...params,
+          uses: partId,
+          part_detail: true,
+          sub_part_detail: true
+        },
+        tableFilters: tableFilters,
+        modelType: ModelType.part,
+        modelField: 'part'
+      }}
+    />
+  );
+}
